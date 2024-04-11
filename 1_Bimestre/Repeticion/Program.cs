29aa994fe2f52bdbfd4e334c795d5803c4b8b4ee
Sherklan <97853791+Sherklan12e@users.noreply.github.com>
2024-04-11 12:01:46 -0300

    Update Program.cs
    14 falta

--- 1_Bimestre/Repeticion/Program.cs
+++ 1_Bimestre/Repeticion/Program.cs
@@ -82,91 +82,141 @@
 
 // Realizar un algoritmo en donde el usuario ingrese cadenas de caracteres de forma indefinida. Si la cadena ingresada es una cadena vacía (salto de línea), se debe informar por pantalla la cantidad de líneas ingresadas.
 
-while()
 
 
 
-//for
-//uso habitual
-// for (int i = 0; i < 5; i++)
-//     Console.WriteLine("Mensaje" + (i + 1));
 
-// for (int i = 0; i < 6; i++)
-// {
-//     Console.WriteLine(i + 1);
-//     Console.WriteLine("Mensaje");
-// }
+// Ejercicio9
 
-// for (int i = 10; i > 6; i--) // i-- equivale a i = i - 1
-// {
-//     Console.WriteLine(i);
-//     Console.WriteLine("Mensaje");
-// }
+// Realizar un algoritmo en donde el usuario ingrese dos números enteros distintos indefinidamente. Se debe informar por pantalla la suma de dichos números siempre y cuando ambos números sean distintos. Si ambos números son iguales debe finalizar el algoritmo.
 
-// for (int i = 0; i <= 10; i = i + 2)
-//     Console.WriteLine(i);
-
-//uso poco usual
-//for (int i = 0; i < 5; i++) ;
-
-//for (; ; );
-
-//while
-// condicion es verdadera
-// while(condicion) => condicion tiene un valor de verdad
-// {
-//      condicion es falsa
-// }
-
-// int i = 10;
-// int j = 1;
-// while (i < 10 && j > 0) //&& = and ; || = or
-// {
-//     Console.WriteLine(i);
-//     i++;
-// }
-
-
-//do-while
-// do {
-//      condicion es falsa
-// } while (condicion) => se ejecuta mientras la condicion es verdadera
-
-// int i = 1;
+// int resultado = 0;
+// int num1=0;
+// int num2=0;
 // do
 // {
-//     Console.WriteLine(i);
-//     i++;
-// } while (i < 10);
+//     num1 = Convert.ToInt32(Console.ReadLine());
+//     num2 = Convert.ToInt32(Console.ReadLine());
 
+//     if (num1 != num2){
+//         resultado = num1 + num2;
+//         Console.WriteLine("Resultado " + resultado);
 
-//Ejercicio 1
-// Realizar un algoritmo que informe por pantalla todos los números enteros del 100 al 200
-// for (int numero = 100; numero <= 200; numero++)
-//     Console.WriteLine(numero);
+//     }
+// }
+// while (num1 != num2);
 
-// for (int numero = 200; numero >= 100; numero--)
-//     Console.WriteLine(numero);
-
-//Ejercicio 5
-// Realizar un algoritmo que informe por pantalla todos los números naturales desde el 1 (uno) hasta 
-// el número natural que el usuario ingrese por teclado.
-// int numero, i = 1;
-// Console.Write("Ingrese un numero: ");
+// Realizar un algoritmo que informe por pantalla todos los números naturales desde el 1 (uno) hasta el número natural que el usuario ingrese por teclado.
+// int numero = 0;
+// int i = 0;
 // numero = Convert.ToInt32(Console.ReadLine());
-
-// while (i < numero)
-// {
-//     Console.WriteLine(i);
-//     i++;
-// }
-
-//Ejercicio 11
-//Hacer un algoritmo para mostrar por pantalla los números del 20 al 200 en orden creciente
-
-// int i = 20;
 // do
 // {
-//     Console.WriteLine(i);
+//     Console.WriteLine(i+1);
+
 //     i++;
-// } while (i <= 200);
+// }
+// while ( numero > i);
+// Hacer un algoritmo para mostrar por pantalla los números del 20 al 200 en orden creciente
+// int numero = 19;
+// do
+// {
+//     Console.WriteLine(numero+1);
+//     numero++;
+// }
+// while (numero < 200);
+
+
+// Realizar un algoritmo que permita al usuario ingresar por teclado la cantidad de agua caída en milímetros (número entero) día a día durante un mes (1 mes = 30 días). Se pide informar por pantalla el número de día en el que llovió más, el número de día en el que llovió menos y el promedio de lluvia caída (en milímetros) durante el mes.
+// int num = 0;
+// int numdia = 0;
+// int numdiamenor = 0;
+// int numeros = 0;
+// int mayor = 0;
+// int menor = 0;
+// int promedio = 0;
+// int diasum = 0;
+
+
+// while ( num < 30){ 
+//     Console.WriteLine("Ingrese la cantidad de agua del dia "+(num+1));
+//     numeros = Convert.ToInt32(Console.ReadLine());
+//     if (num == 0){
+//         menor = numeros;
+//     }
+    
+//     if (numeros > mayor){
+//         mayor = numeros;
+//         numdia = num+1 ;
+//     }
+//     else if (numeros < menor){
+//         menor = numeros;
+//         numdiamenor = num+1;
+//     }
+    
+//     diasum = numeros + diasum;
+//     num++;
+// }
+// promedio = diasum / 30;
+// Console.Write("El dia donde llovio mas fue en " + numdia );
+// Console.Write("El dia menor seria el "+ numdiamenor+" ");
+// Console.WriteLine("El promedio seria en total de "+ promedio);
+
+
+// Realizar un programa que pida la estatura (en metros) y edad de un número indeterminado de personas. Posteriormente se debe informar por pantalla la estatura media de las personas mayores a 21 años y la estatura más alta de las personas menores a 21 años.
+
+// decimal estatura = 0;
+// int edad = 0;
+// decimal promedio = 0;
+// decimal sumatotal = 0;
+// int bandera = 1;
+// decimal  estaturaalta = 0;
+// int persona = 0;
+// while (true){
+//     Console.WriteLine("Ingrese Su estatura de la persona "+ bandera + ": ");
+//     estatura = Convert.ToDecimal(Console.ReadLine());
+//     Console.WriteLine("Ingrese su edad de la persona "+ bandera + ": ");
+//     edad = Convert.ToInt32(Console.ReadLine());
+    
+//     if (estatura == 0  || edad ==0 ){
+//         break;
+//     }
+//     if (edad >21){
+//         sumatotal = estatura + sumatotal;
+//     }else{
+//         if (estatura > estaturaalta){
+//             estaturaalta = estatura;
+//             persona = bandera;
+//         }
+//     }
+//     bandera++;
+// }   
+
+// promedio = sumatotal / bandera;
+// Console.WriteLine("La estatura medio de las personas mayores a 21 es: "+ promedio);
+// Console.WriteLine("La estatura alta de las personas menorese de 21 es: " + persona);
+
+// Realizar un algoritmo que lea una cantidad indefinida de números naturales y se termine la carga con el número cero. Posteriormente informar por pantalla cuál fue el número mayor ingresado y que cantidad de veces fue ingresado.
+int numero1 = 0;
+int bandera = 1;
+int cantidad =0;
+int mayor = 0;
+int index = 0;
+
+while (true){
+    Console.WriteLine("Ingrese el numero : " + bandera);
+    numero1 = Convert.ToInt32(Console.ReadLine());
+    if (numero1 == 0){
+        break;
+    }
+    
+    if (numero1 > mayor){
+        mayor = numero1;
+        cantidad ++;
+    }
+    bandera ++; 
+}
+
+Console.WriteLine("El numero mayor es es " + mayor);
+Console.WriteLine("y la cantidad de veces es: " + cantidad);
+
